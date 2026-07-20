b1f137ed4101f78f804f35c6abb539005a3e0ca8
Prajay <prajay@Riteshs-MacBook-Air.local>
2026-07-20 17:46:46 +0530

    implemented user authenitcation and registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
 # Create your views here.
 
 @login_required
@@ -9,5 +10,29 @@ def dashboard(request):
         'accounts/dashboard.html',
         {
             'section':'dashboard'
+        }
+    )
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save(commit = False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(
+                request,'accounts/register_done.html',{
+                    'new_user' : new_user
+                }
+            )
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(
+        request,
+        'accounts/register.html',{
+            'user_form' : user_form
         }
     )
